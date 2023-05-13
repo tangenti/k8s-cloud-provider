8158a8cdc2ce716df41aea4b0c1462fb8a13dc40
Bowei Du <bowei@google.com>
2023-05-13 10:29:47 -0700

    workflows

---
+++ pkg/cloud/sync/workflow/gc.go
@@ -0,0 +1,145 @@
+/*
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workflow
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/GoogleCloudPlatform/k8s-cloud-provider/pkg/cloud"
+	"github.com/GoogleCloudPlatform/k8s-cloud-provider/pkg/cloud/sync"
+	"google.golang.org/api/compute/v1"
+	"k8s.io/klog/v2"
+)
+
+type GarbageCollectScope string
+
+const (
+	GarbageCollectGlobal   GarbageCollectScope = "Global"
+	GarbageCollectRegional GarbageCollectScope = "Regional"
+)
+
+// GarbageCollectConfig for the garbage collector.
+type GarbageCollectConfig struct {
+	// Scope of the garbage collection operation.
+	Scope GarbageCollectScope
+	// Region to operate on if regional scope. Leave this empty otherwise.
+	Region string
+	// ResourcePrefix is the prefix (e.g. "gke-gw-") used to filter
+	// the resources that are relevant for the garbage collection
+	// operation.
+	ResourcePrefix string
+	// NodeIsLive returns true if the Node should not be deleted.
+	NodeIsLive func(sync.Node) bool
+}
+
+// GarbageCollect resources as identified by config.
+func GarbageCollect(ctx context.Context, cl cloud.Cloud, config *GarbageCollectConfig) error {
+	w := gcWorkflow{cloud: cl, config: config}
+	return w.do(ctx)
+}
+
+type gcWorkflow struct {
+	cloud  cloud.Cloud
+	config *GarbageCollectConfig
+}
+
+func (w *gcWorkflow) do(ctx context.Context) error {
+	var (
+		frules []*compute.ForwardingRule
+		err    error
+	)
+
+	switch w.config.Scope {
+	case GarbageCollectGlobal:
+		frules, err = w.cloud.GlobalForwardingRules().List(ctx, nil)
+		if err != nil {
+			return fmt.Errorf("gc workflow: %w", err)
+		}
+	case GarbageCollectRegional:
+		frules, err = w.cloud.ForwardingRules().List(ctx, w.config.Region, nil)
+		if err != nil {
+			return fmt.Errorf("gc workflow: %w", err)
+		}
+	default:
+		return fmt.Errorf("gc workflow: invalid scope %q", w.config.Scope)
+	}
+
+	// Seed the graph with the forwarding rules.
+	got := sync.NewGraph()
+	for _, fr := range frules {
+		if !strings.HasPrefix(fr.Name, w.config.ResourcePrefix) {
+			continue
+		}
+		id, err := cloud.ParseResourceURL(fr.SelfLink)
+		if err != nil {
+			return fmt.Errorf("gc workflow: %w", err)
+		}
+		node, err := sync.NewNodeByID(id, sync.NodeAttributes{
+			Ownership: sync.OwnershipManaged,
+		})
+		if err := got.AddNode(node); err != nil {
+			return fmt.Errorf("gc workflow: %w", err)
+		}
+	}
+	// Take the transitive closure from the FRs to get the resource graph.
+	if err := sync.TransitiveClosure(ctx, w.cloud, got,
+		sync.TransitiveClosureOnGet(func(node sync.Node) error {
+			if strings.HasPrefix(node.ID().Key.Name, w.config.ResourcePrefix) {
+				node.SetOwnership(sync.OwnershipManaged)
+			} else {
+				node.SetOwnership(sync.OwnershipExternal)
+			}
+			return nil
+		}),
+	); err != nil {
+		return fmt.Errorf("gc workflow: %w", err)
+	}
+
+	// TODO: Make subgraphs live.
+
+	want := sync.NewGraph()
+	for _, node := range got.All() {
+		if w.config.NodeIsLive(node) {
+			// TODO
+		} else {
+			// Mark this node for deletion in the want graph.
+			wantNode := node.NewEmptyNode()
+			wantNode.SetState(sync.NodeDoesNotExist)
+			if err := want.AddNode(wantNode); err != nil {
+				return fmt.Errorf("gc workflow: %w", err)
+			}
+		}
+	}
+
+	klog.Infof("GC want = %+v", want)
+
+	// TODO:
+
+	if err := sync.LocalPlan(got, want); err != nil {
+		return fmt.Errorf("gc worflow: %w", err)
+	}
+
+	for _, node := range want.All() {
+		klog.Infof("[PLAN] node %s: %s", node.ID(), node.LocalPlan())
+	}
+
+	// TODO
+
+	return nil
+}
---
+++ pkg/cloud/sync/workflow/lb.go
@@ -0,0 +1,160 @@
+/*
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package workflow
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleCloudPlatform/k8s-cloud-provider/pkg/cloud"
+	"github.com/GoogleCloudPlatform/k8s-cloud-provider/pkg/cloud/sync"
+	"k8s.io/klog/v2"
+)
+
+// LoadBalancer will perform updates to cloud resources wanted in graph.
+func LoadBalancer(ctx context.Context, c cloud.Cloud, graph *sync.Graph) error {
+	w := lbWorkflow{
+		cloud: c,
+		got:   sync.NewGraph(),
+		want:  graph,
+	}
+	if err := w.plan(ctx); err != nil {
+		return err
+	}
+
+	// XXX
+	for _, node := range w.want.All() {
+		klog.Infof("[PLAN] %v: %s", node.ID(), node.LocalPlan())
+	}
+
+	return w.execute(ctx)
+}
+
+type lbWorkflow struct {
+	cloud cloud.Cloud
+	got   *sync.Graph
+	want  *sync.Graph
+}
+
+func (w *lbWorkflow) plan(ctx context.Context) error {
+	// Assemble the "got" graph. This will get the current state of any
+	// resources and also enumerate any resouces that are currently linked that
+	// are not in the "want" graph.
+	for _, node := range w.want.All() {
+		gotNode := node.NewEmptyNode()
+		w.got.AddNode(gotNode)
+	}
+
+	// Fetch the current resource graph from Cloud.
+	err := sync.TransitiveClosure(ctx, w.cloud, w.got) // TODO: resource_prefix, ownership
+	if err != nil {
+		return err
+	}
+
+	// Copy nodes in "got" that aren't in "want" as not existing so the planner
+	// will mark them for deletion.
+	for _, node := range w.got.All() {
+		if w.want.Resource(node.ID()) != nil {
+			continue
+		}
+		wantNode := node.NewEmptyNode()
+		wantNode.SetState(sync.NodeDoesNotExist)
+		w.got.AddNode(wantNode)
+	}
+
+	// Compute the local plan for each resource.
+	if err := sync.LocalPlan(w.got, w.want); err != nil {
+		return err
+	}
+
+	if err := w.propagateRecreates(); err != nil {
+		return err
+	}
+
+	if err := w.sanityCheck(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// propagateRecreates through inbound references. If a resource needs to be
+// recreated, this means any references will also be affected transitively.
+func (w *lbWorkflow) propagateRecreates() error {
+	var recreateNodes []sync.Node
+	for _, node := range w.want.All() {
+		if node.LocalPlan().Op() == sync.OpRecreate {
+			recreateNodes = append(recreateNodes, node)
+		}
+	}
+
+	done := map[cloud.ResourceMapKey]bool{}
+	for _, node := range recreateNodes {
+		for _, inRefNode := range sync.TransitiveInRefs(w.want, node) {
+			if done[inRefNode.MapKey()] {
+				continue
+			}
+			done[inRefNode.MapKey()] = true
+
+			if inRefNode.Ownership() != sync.OwnershipManaged {
+				return fmt.Errorf("lb workflow: %v planned for recreate, but inRef %v ownership=%s", node.ID(), inRefNode.ID(), inRefNode.Ownership())
+			}
+
+			switch inRefNode.LocalPlan().Op() {
+			case sync.OpUpdate:
+				fallthrough
+			case sync.OpNothing:
+				node.LocalPlan().Set(sync.Action{
+					Operation: sync.OpRecreate,
+					Why:       fmt.Sprintf("Dependency %v is being recreated", node.ID()),
+				})
+			}
+		}
+	}
+	return nil
+}
+
+func (w *lbWorkflow) sanityCheck() error {
+	w.want.ComputeInRefs()
+	for _, node := range w.want.All() {
+		switch node.LocalPlan().Op() {
+		case sync.OpUnknown:
+			return fmt.Errorf("lb workflow: node %v has invalid op %s", node.ID(), node.LocalPlan().Op())
+		case sync.OpDelete:
+			// If A => B; if B is to be deleted, then A must be deleted.
+			for _, refs := range node.InRefs() {
+				if inNode := w.want.Resource(refs.To); inNode == nil {
+					return fmt.Errorf("lb workflow: inRef points to non-existant node %v", refs.To)
+				} else if inNode.LocalPlan().Op() != sync.OpDelete {
+					return fmt.Errorf("lb workflow: %v to be deleted, but inRef %v is not", node.ID(), inNode.ID())
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func (w *lbWorkflow) execute(ctx context.Context) error {
+	// TODO
+	// Create or update BackendServices, HealthChecks. Each BS can be done in
+	// parallel.
+	//
+	// Create/update the rest of the hierarchy. It is preferred to write this
+	// first using a single-threaded code and then add parallelism in stages
+	// rather than implementing a full-featured task graph from the outset.
+	return nil
+}
